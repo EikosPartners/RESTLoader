6bfd07268407378a1772cd4cac5062fdf714a613
DanTanzer <dan.tanzer@eikospartners.com>
2015-07-21 15:03:30 -0400

    First

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# RESTLoader
-Module REST Loading 
+Usage
+---
+
+```sh
+
+$ npm install --save EikosPartners/RESTLoader
+
+```
+
+```js
+var app = express(),
+    routes = [];
+var loader = require("RESTLoader")
+routes.push(require('./Module'));
+loader.load(app,routes);
+
+--------------------------------------------------
+module a sample module
+--------------------------------------------------
+API: {
+
+        '/sampleEndPoint/:id': {
+            type: 'get',
+            desc: 'sample get endpoint',
+            responder: function (req, res, next) {
+                source.get(loader.respond(req, res));
+            }
+        },
+        '/sampleEndPoint/:id': {
+            type: 'post',
+            desc: 'sends a post to an endpoint',
+            responder: function (req, res, next) {
+                source.saveMany(req, next);
+            }
+        }
+};
+
+```
+
+
+```
+
+Linting
+---
+
+Linted with jshint
+
+```sh
+$ npm run lint
+```
+
+Testing
+---
+
+Tested with mocha
+
+```sh
+$ npm test
+```
+
+Documentation
+---
+
+Documented with jsdoc
+
+```sh
+$ npm run docs
+```
+
---
+++ index.js
@@ -0,0 +1,47 @@
+var _ = require('lodash'),
+    formatted = '' +
+    '<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js' +
+    '/8.6/highlight.min.js"></script>' +
+    '<script>hljs.initHighlightingOnLoad();</script>' +
+    '<style>' +
+    'body{background-color:#1C2A39}' +
+    '.hljs{color:#6C7A89;font-size:1.2em}' +
+    '.hljs-attribute{color:#2ECC71}' +
+    '.hljs-string{color:#F39C12}' +
+    '.hljs-number{color:#3498DB}' +
+    '.hljs-literal{color:#9B59B6}' +
+    '</style>';
+
+module.exports = {
+    respond: function ( req, res, format ) {
+        return _.once(function ( error, data ) {
+            if (req.query.formatted !== void 0 || format) {
+                res.send(formatted +
+                    '<pre><code class="json">' +
+                    JSON.stringify(data, null, 4) +
+                    '</code></pre>');
+            }
+            if (error) {
+                data = {error: error};
+                res.status(404).send(data);
+            } else {
+                res.json(data);
+            }
+        });
+    },
+    load: function(app, routes){
+        var key;
+        routes.forEach(function ( route ) {
+            var API = route.API;
+            for (key in API) {
+                console.log('registering ' + key + ' as ' + API[key].type);
+                app[API[key].type](key, API[key].responder);
+                //if (API[key].type !== 'get' && API[key].type !== 'use') {
+                //    console.log('registering ' + key + ' as get');
+                //    app.get(key, API[key].responder);
+                //}
+            }
+        });
+    }
+};
+
